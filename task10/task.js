5dbc202faefb250f21e095c82d2f6e14d0aa6022
Pavithra T <160224663+pavixtokyo@users.noreply.github.com>
2024-12-12 14:11:54 +0530

    Add files via upload

---
+++ task10/task.js
@@ -0,0 +1,12 @@
+function multiplyBy(){
+    const num1=parseFloat(document.getElementById("num1").value);
+    const num2=parseFloat(document.getElementById("num2").value);
+    const result=num1*num2;
+    document.getElementById("result").textContent=result;
+}
+function divideBy(){
+    const num1=parseFloat(document.getElementById("num1").value);
+    const num2=parseFloat(document.getElementById("num2").value);
+    const result=num1/num2; 
+    document.getElementById("result").textContent=result;
+}
